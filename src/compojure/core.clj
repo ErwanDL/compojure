34572ada11c27b20c3d4b5098866945772d51a33
Erwan <erwan.delepinau@free.fr>
2020-11-11 00:25:39 +0100

    interpreter: now callable via -main function of project

--- src/compojure/core.clj
+++ src/compojure/core.clj
@@ -1,7 +1,20 @@
 (ns compojure.core
-  (:gen-class))
+  (:gen-class)
+  (:require [compojure.parser :refer [parse]]
+            [compojure.e-prog :refer [ast-to-program]]
+            [compojure.interpreter :refer [eval-e-prog]]))
+
+
+(defn interpret-e-program
+  [source-code args]
+  (-> source-code
+      (parse)
+      (ast-to-program)
+      (eval-e-prog args)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println
+   (interpret-e-program (first args)
+                        (map #(Integer/parseInt %) (rest args)))))
+
