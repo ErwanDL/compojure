e4d0243fb9708f00b20171ed0c9b1533b197f640
Erwan <erwan.delepinau@free.fr>
2020-11-16 19:56:35 +0100

    refactor parser_test, start building test_framework

--- test/compojure/parser_test.clj
+++ test/compojure/parser_test.clj
@@ -1,66 +1,42 @@
 (ns compojure.parser-test
   (:require [clojure.test :refer [deftest is testing]]
-            [clojure.java.io :as io]
             [compojure.parser :refer [e-parser parse]]
-            [instaparse.core :as insta]))
+            [instaparse.core :as insta]
+            [compojure.test-framework :refer [snippet-files-in]]))
 
 
-(defn successful-parse? [snippet-name]
+(defn successful-unambiguous-parse? [snippet-file]
   ;; If insta/parses produces 0 parse tree,
   ;; it means it failed to parse the input.
   ;; If insta/parses produces strictly more than 1 parse tree,
   ;; it means that the grammar is ambiguous.
   (= 1 (count (insta/parses e-parser
-                            (slurp (io/resource
-                                    (str "e_snippets/" snippet-name)))))))
+                            (slurp snippet-file)))))
+
+(def invalid-snippet-names
+  #{"lexerror.e"
+    "syntaxerror1.e"
+    "syntaxerror2.e"})
+
+(def group-invalid-valid-basic-files
+  (group-by (fn [file]
+              (some #(= % (.getName file)) invalid-snippet-names))
+            (snippet-files-in "basic")))
+(def valid-basic-files
+  (get group-invalid-valid-basic-files nil))
+
+(def invalid-basic-files
+  (get group-invalid-valid-basic-files true))
+
 
 (deftest parser-basic-test
+  (testing "On syntactically valid programs"
+    (dorun (map #(is (successful-unambiguous-parse? %)) valid-basic-files)))
   (testing "On syntactically invalid programs"
-    (is (not (successful-parse? "basic/lexerror.e")))
-    (is (not (successful-parse? "basic/syntaxerror1.e")))
-    (is (not (successful-parse? "basic/syntaxerror2.e"))))
-  (testing "On syntactically correct programs"
-    (is (successful-parse? "basic/ret0.e"))
-    (is (successful-parse? "basic/uninitialized.e"))
-    (is (successful-parse? "basic/just_a_variable_37.e"))
-    (is (successful-parse? "basic/2xpy.e"))
-    (is (successful-parse? "basic/x-is-what.e"))
-    (is (successful-parse? "basic/ret60.e"))
-    (is (successful-parse? "basic/ret20.e"))
-    (is (successful-parse? "basic/print_int.e"))
-    (is (successful-parse? "basic/useless-assigns.e"))
-    (is (successful-parse? "basic/loop.e"))
-    (is (successful-parse? "basic/loop2.e"))
-    (is (successful-parse? "basic/loop3.e"))
-    (is (successful-parse? "basic/loop4.e"))
-    (is (successful-parse? "basic/prime.e"))
-    (is (successful-parse? "basic/sum_ints_upto_10.e"))
-    (is (successful-parse? "basic/sum_ints_upto_n.e"))
-    (is (successful-parse? "basic/mulmulmul.e"))
-    (is (successful-parse? "basic/fibbetter.e"))
-    (is (successful-parse? "basic/35gt12.e"))
-    (is (successful-parse? "basic/arg-test.e"))
-    (is (successful-parse? "basic/a_19.e"))
-    (is (successful-parse? "basic/lots-of-regs.e"))
-    (is (successful-parse? "basic/range_0_10.e"))
-    (is (successful-parse? "basic/range_10_0_then_dec0.e"))
-    (is (successful-parse? "basic/gcd.e"))
-    (is (successful-parse? "basic/a_simple_variable_23.e"))
-    (is (successful-parse? "basic/ret21.e"))
-    (is (successful-parse? "basic/dec_to_0.e"))
-    (is (successful-parse? "basic/comment.e"))
-    (is (successful-parse? "basic/comment++.e"))
-    (is (successful-parse? "basic/muldivmod.e"))
-    (is (successful-parse? "basic/1_or_3a.e"))
-    (is (successful-parse? "basic/double.e"))
-    (is (successful-parse? "basic/1_or_4.e"))
-    (is (successful-parse? "basic/fac.e"))))
+    (dorun (map #(is (not (successful-unambiguous-parse? %))) invalid-basic-files))))
 
 (deftest parser-if-else-test
-  (is (successful-parse? "if_else/no_braces.e"))
-  (is (successful-parse? "if_else/dangling_else.e"))
-  (is (successful-parse? "if_else/if_while.e"))
-  (is (successful-parse? "if_else/else_open_statement.e"))
+  (dorun (map #(is (successful-unambiguous-parse? %)) (snippet-files-in "if_else")))
   (testing "Dangling else is correctly paired with closest if"
     (is (= [:S
             [:FUNDEF
---
+++ test/compojure/test_framework.clj
@@ -0,0 +1,9 @@
+(ns compojure.test-framework
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn snippet-files-in [folder-name]
+  (filter #(string/ends-with? % ".e")
+          (rest (file-seq (io/file
+                           (io/resource
+                            (str "e_snippets/" folder-name)))))))
