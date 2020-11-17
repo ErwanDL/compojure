0297e69e1d2ae34fb924c3544e341c9f2ed8c91b
Erwan <erwan.delepinau@free.fr>
2020-11-18 00:28:29 +0100

    test CFGStatement's execute

---
+++ test/compojure/cfg/cfg_exec_test.clj
@@ -0,0 +1,44 @@
+(ns compojure.cfg.cfg-exec-test
+  (:require [clojure.test :refer [deftest is]]
+            [compojure.cfg.cfg-lang :as cfgl]
+            [compojure.test-utils]
+            [compojure.e-lang.e-lang :as e]
+            [compojure.cfg.cfg-exec :refer [execute]]))
+
+(deftest execute-assignment-test
+  (is (= [1 {"a" 5}]
+         (execute (cfgl/->Assignment (e/->Identifier "a")
+                                     (e/->BinaryExpr :SUM
+                                                     (e/->Identifier "a")
+                                                     (e/->Int 2))
+                                     1)
+                  {"a" 3}))))
+
+(deftest execute-condition-test
+  (is (= [1 {"a" 3}]
+         (execute (cfgl/->Condition (e/->Int 1)
+                                    1
+                                    3)
+                  {"a" 3})))
+  (is (= [3 {"a" 3}]
+         (execute (cfgl/->Condition (e/->Int 0)
+                                    1
+                                    3)
+                  {"a" 3}))))
+
+(deftest execute-print-test
+  (binding [*out* (java.io.StringWriter.)]
+    (is (= [1 {}]
+           (execute (cfgl/->Print (e/->Int 5) 1) {})))
+    (is (= "5\n" (str *out*)))))
+
+(deftest execute-return-test
+  (is (ex-info-thrown-with-data?
+       {:type :return-encountered
+        :retval 0
+        :final-state {"a" 0}}
+       (execute (cfgl/->Return (e/->Identifier "a")) {"a" 0}))))
+
+(deftest execute-nop-test
+  (is (= [1 {"a" 5}]
+         (execute (cfgl/->Nop 1) {"a" 5}))))
