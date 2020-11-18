99b9596c46317398e46eb760024d9440c01f2c33
Erwan <erwan.delepinau@free.fr>
2020-11-18 11:06:01 +0100

    improve command line help

--- src/compojure/core.clj
+++ src/compojure/core.clj
@@ -5,30 +5,35 @@
             [clojure.tools.cli :refer [parse-opts]]))
 
 (def cli-options
-  [["-s" "--string" "Directly interpret the string passed as last argument, 
-                     instead of considering it as a filepath"]
-   ["-r" "--representation REPR" "Define what intermediate representation
-                                  to interpret (one of \"e\", \"cfg\"),
-                                  defaults to \"e\""
+  [["-s" "--string" (str "Directly interpret the string passed as "
+                         "last argument, instead of considering it "
+                         "as a filepath")]
+   ["-r" "--representation REPR" (str "Define what intermediate "
+                                      "representation to interpret "
+                                      "(one of \"e\", \"cfg\"), "
+                                      "defaults to \"e\"")
     :default "e"
-    :validate [(fn [val] (some #{val} ["e" "cfg"])) "Representation must
-                                                     be one of \"e\", \"cfg\""]]
-   ["-h" "--help"]])
+    :validate [(fn [val] (some #{val} ["e" "cfg"]))
+               "Representation must be one of \"e\", \"cfg\""]]
+   ["-h" "--help" "Show command line help"]])
 
 (defn -main [& args]
   (println
    (let [{errors :errors
           argmts :arguments
-          opts :options} (parse-opts args cli-options)]
-     (if errors
-       (first errors)
-       (if (< (count argmts) 1)
-         "Please provide a string or the path to a file to interpret"
-         (let [script (first argmts)
-               src (if (:string opts)
-                     script
-                     (slurp script))
-               int-args (map #(Integer/parseInt %) (rest argmts))]
-           (case (:representation opts)
-             "e" (e-interpreter/interpret src int-args)
-             "cfg" (cfg-interpreter/interpret src int-args))))))))
+          opts :options
+          summ :summary} (parse-opts args cli-options)]
+     (if (:help opts)
+       summ
+       (if errors
+         (first errors)
+         (if (< (count argmts) 1)
+           "Please provide a string or the path to a file to interpret"
+           (let [script (first argmts)
+                 src (if (:string opts)
+                       script
+                       (slurp script))
+                 int-args (map #(Integer/parseInt %) (rest argmts))]
+             (case (:representation opts)
+               "e" (e-interpreter/interpret src int-args)
+               "cfg" (cfg-interpreter/interpret src int-args)))))))))
